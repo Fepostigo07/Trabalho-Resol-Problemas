6ea1a31ba1af2767d42b3c457bfc11c836935f5a
LUCAS00yvl <gcorreia173@gmail.com>
2026-08-07 12:21:17 -0300

    Fim da aula

---
+++ Trabalho Resol Problemas/src/Personagem.java
@@ -0,0 +1,7 @@
+ public abstract class Personagem {
+    private String nome;
+    private Sexo Sexo;
+    private double altura;
+
+    public abstract String apresentar();
+}
--- Trabalho Resol Problemas/src/SuperHeroi.java
+++ Trabalho Resol Problemas/src/SuperHeroi.java
@@ -1,10 +1,28 @@
 // NÃO ALTERE ANTES DE LER OS PASSOS
 public class SuperHeroi {
+<<<<<<< Updated upstream
     public String nome;
     public int sexo;                // 1=Mulher, 2=Homem, 3=Outro, 4=Prefiro não dizer
     public double altura;
     public boolean identidadeSecreta;
     public String[] habilidades;
+=======
+    private String nome;
+    private double altura;
+    private boolean identidadeSecreta;
+    Sexo sexo;
+    private String[] habilidades;
+
+    public SuperHeroi(String nome, double altura) {
+        if (nome == null){
+            throw new IllegalArgumentException("O nome não pode ser vazio");
+        }
+        this.nome = nome;
+        this.altura = altura;
+    }
+
+
+>>>>>>> Stashed changes
 
     public void mostrar() {
         System.out.println(nome + " - " + sexo + " - " + altura);
